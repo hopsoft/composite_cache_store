2ac0f47ff74f42d30e3e15caf3f9cbf3b223c840
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-03-10 13:41:59 -0700

    bundle gem turbo_boost-cache

---
+++ lib/turbo_boost/cache.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "cache/version"
+
+module TurboBoost
+  module Cache
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/turbo_boost/cache/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module TurboBoost
+  module Cache
+    VERSION = "0.1.0"
+  end
+end
