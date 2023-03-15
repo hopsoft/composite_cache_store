89877a88be57a8e8c4fae611fa60abc595e311a3
Julian Rubisch <julian@julianrubisch.at>
2023-03-15 14:43:30 +0100

    chore: Add cache store behaviors from active support cache tests

---
+++ test/behaviors/cache_increment_decrement_behavior.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module CacheIncrementDecrementBehavior
+  def test_increment
+    key = SecureRandom.uuid
+    @cache.write(key, 1, raw: true)
+    assert_equal 1, @cache.read(key, raw: true).to_i
+    assert_equal 2, @cache.increment(key)
+    assert_equal 2, @cache.read(key, raw: true).to_i
+    assert_equal 3, @cache.increment(key)
+    assert_equal 3, @cache.read(key, raw: true).to_i
+
+    missing = @cache.increment(SecureRandom.alphanumeric)
+    assert_equal 1, missing
+    missing = @cache.increment(SecureRandom.alphanumeric, 100)
+    assert_equal 100, missing
+  end
+
+  def test_decrement
+    key = SecureRandom.uuid
+    @cache.write(key, 3, raw: true)
+    assert_equal 3, @cache.read(key, raw: true).to_i
+    assert_equal 2, @cache.decrement(key)
+    assert_equal 2, @cache.read(key, raw: true).to_i
+    assert_equal 1, @cache.decrement(key)
+    assert_equal 1, @cache.read(key, raw: true).to_i
+
+    missing = @cache.decrement(SecureRandom.alphanumeric)
+    assert_equal @cache.is_a?(ActiveSupport::Cache::MemCacheStore) ? 0 : -1, missing
+    missing = @cache.decrement(SecureRandom.alphanumeric, 100)
+    assert_equal @cache.is_a?(ActiveSupport::Cache::MemCacheStore) ? 0 : -100, missing
+  end
+end
